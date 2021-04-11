3abd99cc53b57d0c6254c9f65f2e66b8fc59a64b
aditya6521 <74780249+aditya6521@users.noreply.github.com>
2021-04-11 20:31:00 +0530

    Node basics

---
+++ app.js
@@ -0,0 +1,39 @@
+//console.log("Hello");
+//console.log(module);
+
+//const logger=require("./logger.js");
+//console.log(logger);
+//logger.log("aditya");
+
+
+// learning path module
+
+/*const path=require("path");
+
+var pathObj=path.parse(__filename);
+console.log(pathObj);*/
+
+// learning OS module
+
+/*const os=require("os");
+
+var freeMemory=os.freemem();
+var TotalMemory=os.totalmem();
+console.log("Your freee memory is  "+freeMemory);
+console.log("Your Total memory is  "+TotalMemory); */
+
+//filesystem module
+
+const fs=require("fs");
+
+//synchronous
+//const files=fs.readdirSync("./");
+//console.log(files);
+
+//asynchronous
+fs.readdir("./", function(err,files){
+    if(err)
+    console.log("error",err);
+    else
+    console.log("result",files);
+});
